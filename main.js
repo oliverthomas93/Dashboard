2f04043a23adb9921714aa0d08665ad10f72628c
Oliver Thomas <63861649+oliverthomas93@users.noreply.github.com>
2020-10-25 09:30:44 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,40 @@
-window.addEventListener('load', () => {
-
-    //Random Photo Generator
-    let welcome = document.querySelector('#welcome');
-    let pics = ["url('./clay-banks-W02d-kJXO5I-unsplash.jpg')", "url('./daniel-sessler-4r3iFXolFec-unsplash.jpg')", "url('./de-an-sun-JWh255yVuhM-unsplash.jpg')", "url('./deglee-degi-SQoH2ZQd80E-unsplash.jpg')", "url('./egor-myznik-EQl-Gjg-rx4-unsplash.jpg')", "url('./fabrizio-conti-E2GXbvCJc58-unsplash.jpg')", "url('./ingmar-i0qX9Y5z2dE-unsplash.jpg')", "url('./ingmar-nW1HHttYIEg-unsplash.jpg')", "url('./kristaps-ungurs-DW8plOVYzbo-unsplash.jpg')", "url('./kristjan-kotar-6vTtNeq9BWk-unsplash.jpg')", "url('./leon-skibitzki-xzQvrNFMZuw-unsplash.jpg')", "url('./massimiliano-morosinotto-k1nok6_4uq8-unsplash.jpg')", "url('./nikolay-vorobyev-f9lFoW_EL1I-unsplash.jpg')", "url('./xandro-vandewalle-yzGPcKZXeks-unsplash.jpg')"];
-    let randNum = Math.floor(Math.random() * pics.length);
-    welcome.style.backgroundImage = pics[randNum];
-
-    //Greeting Generator
-    let greetingMessage = document.querySelector('#greeting-message');
-    let time = new Date();
-    if (time.getHours() < 12) {
-        greetingMessage.textContent = 'Good Morning Oliver.';
-    } else if (time.getHours() < 17) {
-        greetingMessage.textContent = 'Good Afternoon Oliver.';
-    } else {
-        greetingMessage.textContent = 'Good Evening Oliver.';
-    };
-
-    //Date and Time 
-    function dateTime() {
-        let date = new Date();
-        let h = date.getHours();
-        let m = date.getMinutes();
-        let s = date.getSeconds();
-        m = (m < 10) ? "0" + m : m;
-        s = (s < 10) ? "0" + s : s;
-        let liveTime = `${h}:${m}:${s}`;
-        document.querySelector('#date').textContent = date.toDateString();
-        document.querySelector('#live-time').textContent = liveTime;
-        setTimeout(dateTime, 1000);
-    }
-    dateTime();
-
-    //Quote Generator
-    
+$(document).ready(function() {
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            let hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
 });
 
+let menu = document.querySelector('#menu');
+let nav = document.querySelector('nav');
+let hidden = document.querySelector('#hidden-nav');
+let link1 = document.querySelector('#link1');
+let link2 = document.querySelector('#link2');
+let link3 = document.querySelector('#link3');
+let link4 = document.querySelector('#link4');
+
+let topNav = () => {
+    if (nav.style.background === "black") {
+        nav.style.background = "linear-gradient(black, transparent)";
+    } else {
+        nav.style.background = "black";
+}
+    if (hidden.style.display === "block") {
+        hidden.style.display = "none";
+    } else {
+        hidden.style.display = "block";
+    }
+}
+
+menu.addEventListener('click', topNav);
+link1.addEventListener('click', topNav);
+link2.addEventListener('click', topNav);
+link3.addEventListener('click', topNav);
+link4.addEventListener('click', topNav);
